1c7d07901ceb159d3bd0f7821ee82d207d05411a
Aleksandr Stukalov <stukalovsaleksandrs@gmail.com>
2022-10-06 17:22:20 +0300

    Added wall printing according to their presense in dirs slice

--- exercises/main.go
+++ exercises/main.go
@@ -21,11 +21,27 @@ func fill(dirs [][]byte) [][]byte {
 	// Filling map with "rooms"
 	for i, j := 0, 1; i < len(dirs); i, j = i+1, j+3 {
 		for k, w := 0, 1; k < len(dirs[i]); k, w = k+1, w+3 {
-			maze[j][w] = ' '   // Middle
-			maze[j-1][w] = '-' // North
-			maze[j][w+1] = '|' // East
-			maze[j+1][w] = '-' // South
-			maze[j][w-1] = '|' // East
+			maze[j][w] = ' '       // Middle
+			if dirs[i][k]&N != N { // North
+				maze[j-1][w] = '-'
+			} else {
+				maze[j-1][w] = ' '
+			}
+			if dirs[i][k]&E != E { // East
+				maze[j][w+1] = '|'
+			} else {
+				maze[j][w+1] = ' '
+			}
+			if dirs[i][k]&S != S { // South
+				maze[j+1][w] = '-'
+			} else {
+				maze[j+1][w] = ' '
+			}
+			if dirs[i][k]&W != W { // West
+				maze[j][w-1] = '|'
+			} else {
+				maze[j][w-1] = ' '
+			}
 
 			maze[j-1][w-1] = '+' // NorthWest
 			maze[j-1][w+1] = '+' // NorthEast
@@ -47,10 +63,15 @@ func print(maze [][]byte) {
 }
 
 func main() {
-	var n, m uint64 = 5, 20
-	dirs := make([][]byte, n)
-	for i := range dirs {
-		dirs[i] = make([]byte, m)
+	// var n, m uint64 = 2, 3
+	// dirs := make([][]byte, n)
+	// for i := range dirs {
+	// 	dirs[i] = make([]byte, m)
+	// }
+
+	var dirs [][]byte = [][]byte{
+		{0b0010, 0b1100, 0b0000},
+		{0b0010, 0b1011, 0b1000},
 	}
 
 	maze := fill(dirs)
