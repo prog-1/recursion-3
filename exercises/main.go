c589adc519316c7559d2568cd21806fe676c2fca
Aleksandr Stukalov <stukalovsaleksandrs@gmail.com>
2022-10-07 18:07:48 +0300

    Implemented maze printing

--- exercises/main.go
+++ exercises/main.go
@@ -2,6 +2,7 @@ package main
 
 import "fmt"
 
+// 0 - wall    1 - way
 const (
 	N = 1 << iota
 	E
@@ -9,63 +10,60 @@ const (
 	W
 )
 
-func print(dirs [][]byte) {
-	n, m := len(dirs), len(dirs[0])
-
-	// Creating a map, which will be displayed on terminal
-	maze := make([][]byte, n*3)
-	for i := range maze {
-		maze[i] = make([]byte, m*3)
+func print(maze [][]byte) {
+	// Creating char array, which will be displayed on screen:
+	output := make([][]byte, len(maze)*3)
+	for i := range output {
+		output[i] = make([]byte, len(maze[0])*3)
 	}
 
-	// Filling map with "rooms"
-	for i, j := 0, 1; i < len(dirs); i, j = i+1, j+3 {
-		for k, w := 0, 1; k < len(dirs[i]); k, w = k+1, w+3 {
-			maze[j][w] = ' '       // Middle
-			if dirs[i][k]&N != N { // North
-				maze[j-1][w] = '-'
+	// Filling output array:
+	for i, k := 0, 1; i < len(maze); i, k = i+1, k+3 {
+		for j, w := 0, 1; j < len(maze[i]); j, w = j+1, w+3 {
+			output[k][w] = ' '     // Middle
+			output[k-1][w-1] = '+' // NorthWest
+			output[k-1][w+1] = '+' // NorthEast
+			output[k+1][w+1] = '+' // SouthEash
+			output[k+1][w-1] = '+' // SouthWest
+
+			cell := maze[i][j]
+			if cell&N == N {
+				output[k-1][w] = ' '
 			} else {
-				maze[j-1][w] = ' '
+				output[k-1][w] = '-'
 			}
-			if dirs[i][k]&E != E { // East
-				maze[j][w+1] = '|'
+			if cell&E == E {
+				output[k][w+1] = ' '
 			} else {
-				maze[j][w+1] = ' '
+				output[k][w+1] = '|'
 			}
-			if dirs[i][k]&S != S { // South
-				maze[j+1][w] = '-'
+			if cell&S == S {
+				output[k+1][w] = ' '
 			} else {
-				maze[j+1][w] = ' '
+				output[k+1][w] = '-'
 			}
-			if dirs[i][k]&W != W { // West
-				maze[j][w-1] = '|'
+			if cell&W == W {
+				output[k][w-1] = ' '
 			} else {
-				maze[j][w-1] = ' '
+				output[k][w-1] = '|'
 			}
 
-			maze[j-1][w-1] = '+' // NorthWest
-			maze[j-1][w+1] = '+' // NorthEast
-			maze[j+1][w+1] = '+' // SouthEash
-			maze[j+1][w-1] = '+' // SouthWest
 		}
 	}
 
-	printMaze(maze)
-}
-func printMaze(maze [][]byte) {
-	for i := range maze {
-		for j := range maze[i] {
-			fmt.Print(string(maze[i][j]))
+	// Printing output array:
+	for i := range output {
+		for j := range output[i] {
+			fmt.Print(string(output[i][j]))
 		}
 		fmt.Println()
 	}
 }
 
 func main() {
-	var n, m uint64 = 2, 3
-	dirs := make([][]byte, n)
-	for i := range dirs {
-		dirs[i] = make([]byte, m)
+	var maze [][]byte = [][]byte{
+		{0b0010, 0b1100, 0b0000},
+		{0b0010, 0b1011, 0b1000},
 	}
-	print(dirs)
+	print(maze)
 }
