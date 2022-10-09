a6bc1e30fb4b79b4543b7d5b444ec570515afec0
Ilya <imolchanov210@gmail.com>
2022-10-09 20:34:10 +0300

    maze and solver done

--- main.go
+++ main.go
@@ -39,36 +39,36 @@ var dirUpd = map[Dir]func(col int, row int) (ncol, nrow int){
 	E: func(c int, r int) (int, int) { return c + 1, r },
 }
 
-func cycle(e [][]Dir, end Coord, way []Coord) []Coord {
+func cycle(e [][]Dir, end Coord, way []Coord, visited map[Coord]bool) []Coord {
 	var dirs [4]Dir
 	perm := rand.Perm(4)
-	// fmt.Println(way)
 	for i := range perm {
 		dirs[i] = allDirs[perm[i]]
 	}
 	for _, d := range dirs {
-		// fmt.Println(d, "d")
-		// fmt.Println(e[way[len(way)-1].X][way[len(way)-1].Y])
 		if e[way[len(way)-1].Y][way[len(way)-1].X]&d == 0 {
 			continue
 		}
 		ncol, nrow := dirUpd[d](way[len(way)-1].X, way[len(way)-1].Y)
-		if len(way) > 1 {
-			if ncol == way[len(way)-2].X && nrow == way[len(way)-2].Y {
-				continue
-			}
+		if ncol < 0 || ncol >= len(e[0]) || nrow < 0 || nrow >= len(e) {
+			continue
+		}
+		if v := visited[Coord{ncol, nrow}]; v {
+			continue
+		} else {
+			visited[Coord{ncol, nrow}] = true
 		}
 
 		way = append(way, Coord{ncol, nrow})
 		if way[len(way)-1].X == end.X && way[len(way)-1].Y == end.Y {
 			return way
 		}
-		return cycle(e, end, way)
+		return cycle(e, end, way, visited)
 
 	}
 
 	way = way[:len(way)-1]
-	return cycle(e, end, way)
+	return cycle(e, end, way, visited)
 }
 func AnyPath(e [][]Dir, start, end Coord) []Coord {
 	var way []Coord
@@ -76,7 +76,9 @@ func AnyPath(e [][]Dir, start, end Coord) []Coord {
 	if start.X == end.X && start.Y == end.Y {
 		return nil
 	}
-	return cycle(e, end, way)
+	visited := make(map[Coord]bool)
+	visited[Coord{0, 0}] = true
+	return cycle(e, end, way, visited)
 }
 
 func HasDir(e [][]Dir, row, col int, d Dir) bool {
@@ -91,19 +93,12 @@ func HasDir(e [][]Dir, row, col int, d Dir) bool {
 
 var (
 	allDirs = []Dir{N, W, E, S}
-	// dirUpd  = map[Dir]func(col int, row int) (ncol, nrow int){
-	// 	N: func(c int, r int) (int, int) { return c, r - 1 },
-	// 	S: func(c int, r int) (int, int) { return c, r + 1 },
-	// 	W: func(c int, r int) (int, int) { return c - 1, r },
-	// 	E: func(c int, r int) (int, int) { return c + 1, r },
-	// }
 	dirOpos = map[Dir]Dir{N: S, S: N, W: E, E: W}
 )
 
 func genExits(e [][]Dir, col, row int) {
 	var dirs [4]Dir
 	perm := rand.Perm(4)
-	fmt.Println(perm)
 	for i := range perm {
 		dirs[i] = allDirs[perm[i]]
 	}
@@ -125,29 +120,66 @@ func genExits(e [][]Dir, col, row int) {
 
 }
 
-func output(e [][]Dir) {
-	for i := range e {
-		for j := range e[i] {
-			if 
+func output(e [][]Dir, m map[Coord]tag) {
+	var str string
+	for i := 0; i <= len(e)*6+2; i++ {
+		str += "#"
+	}
+	fmt.Println(str)
+	var tmph, tmpv string
+	for i, v := range e {
+		tmph += "###"
+		tmpv += "###"
+		for j, v1 := range v {
+
+			if _, ok := m[Coord{j, i}]; ok {
+				tmph += " . "
+			} else {
+				tmph += "   "
+			}
+
+			if v1&2 != 0 {
+				tmpv += "   "
+			} else {
+				tmpv += "###"
+			}
+
+			if v1&8 != 0 {
+				if _, ok := m[Coord{j, i}]; ok {
+					tmph += " . "
+				} else {
+					tmph += "   "
+				}
+
+			} else {
+				tmph += "###"
+			}
+			tmpv += "###"
+
 		}
+		fmt.Println(tmph)
+		fmt.Println(tmpv)
+		tmph = ""
+		tmpv = ""
+	}
 
 }
-}
+
+type tag struct{}
+
 func main() {
 	rand.Seed(time.Now().UnixNano())
-
-	const rows, cols = 5, 5
+	const rows, cols = 20, 20
 	exits := make([][]Dir, rows)
 	for i := range exits {
 		exits[i] = make([]Dir, cols)
 	}
 	genExits(exits, 0, 0)
-	output(exits)
-	for i := range exits {
-		for j := range exits[i] {
-			fmt.Printf("%3d", exits[i][j])
-		}
-		fmt.Println()
+	s := AnyPath(exits, Coord{0, 0}, Coord{len(exits) - 1, len(exits[0]) - 1})
+
+	m := make(map[Coord]tag)
+	for _, v := range s {
+		m[v] = tag{}
 	}
-	fmt.Println(AnyPath(exits, Coord{0, 0}, Coord{len(exits) - 1, len(exits[0]) - 1}))
+	output(exits, m)
 }
